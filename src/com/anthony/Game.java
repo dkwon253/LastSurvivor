29b24436809f47e8059883b43b9a67add0ae5b94
AdanielJohnson91 <dan.johnson1022@gmail.com>
2021-02-17 12:07:24 -0600

    board updated

--- src/com/anthony/Game.java
+++ src/com/anthony/Game.java
@@ -7,67 +7,61 @@ import java.awt.event.ActionListener;
 
 public class Game implements ActionListener {
     private JFrame frame;
-    private Second second;
+    private Board board;
     private Container container;
     private CardLayout card;
 
     public Game(String name){
         card = new CardLayout();
         frame = new JFrame(name);
-        second = new Second();
+        board = new Board();
         container = frame.getContentPane();
     }
-
 
     public void showGame(){
         instantiate();
         frame.pack();
         frame.setVisible(true);
     }
-
+    /**
+     * Set Frame to CardLayout; this will allow for switching of components
+     * addComponentToPane will add all components into our application.
+     */
     public void instantiate(){
+
         container.setLayout(card);
         addComponentsToPane();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
 
 
     public void addComponentsToPane(){
-        second = new Second();
         JButton button = new JButton("Button");
         button.setActionCommand("Switch");
         button.addActionListener(this);
-        second.add(button, BorderLayout.SOUTH);
         JButton button2 = new JButton("Welcome, Click to get started!");
         button2.setSize(50,50);
         button2.setActionCommand("Switch");
         button2.addActionListener(this);
         container.add(button2);
-        container.add(second);
+        container.add(board);
     }
     public void addComponentToGame(Component component){
         container.add(component);
     }
 
-    public void setCard(CardLayout card) {
-        this.card = card;
-    }
-
-    public void setGame(Game game) {
-
-    }
-
-    public void setContainer(Container container) {
-        this.container = container;
-    }
-
+    /**
+     * action performed will look for events that are set in the add components
+     * method. "requestFocus" is needed to switch between screens without losing
+     * control over a character.
+     * @param e
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("Switch")){
             card.next(container);
+            board.requestFocus();
         }
-        second.requestFocus();
     }
 
 
--- src/com/anthony/Hero.java
+++ src/com/anthony/Hero.java
@@ -1,168 +1,54 @@
 package com.anthony;
 
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 
-public class Hero extends Ellipse2D implements KeyListener, ActionListener {
-    private double x;
-    private double y;
-    private double width;
-    private double height;
-    private boolean isEmpty;
-    private double velX;
-    private double velY;
+public class Hero extends JPanel  {
+    private Image image;
+    private int x;
+    private int y;
+    private int vX = 0;
+    private int vY = 0;
 
 
-    private Frame frame;
-
-
-    public Hero(double x, double y, double width,double height){
-        setFrame(x,y,width,height);
-    }
-
-    @Override
-    public double getX() {
-        return 0;
-    }
-
-    @Override
-    public double getY() {
-        return 0;
-    }
-
-    @Override
-    public double getWidth() {
-        return 0;
-    }
-
-    @Override
-    public double getHeight() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public void setFrame(double x, double y, double w, double h) {
-    this.x=x;
-    this.y=y;
-    this.width=w;
-    this.height=h;
-    }
-
-    @Override
-    public Rectangle2D getBounds2D() {
-        return null;
-    }
-
-    static class Double extends Ellipse2D.Double{
-        public Double(double x, double y, double w, double h) {
-            super(x, y, w, h);
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        frame.repaint();
-
-        /*if(x < 0 || x > 910){
-            velX = -velX;
-        }
-        if(y < 0 || y > 850){
-            velY = -velY;
-        }*/
-        x += velX;
-        y += velY;
+    public Hero(String filename){
+        this.image = new ImageIcon(filename).getImage();
 
     }
 
-    public void up(){
-        velY= -1.5;
-        velX = 0;
+    public void paint(Graphics2D g2){
+        g2.drawImage(image,  x, y, null);
     }
 
-    public void down(){
-        velY = 1.5;
-        velX = 0;
+    @Override public int getX() {
+        return x;
     }
 
-    public void left(){
-        velX = -1.5;
-        velY = 0;
+    @Override public int getY() {
+        return y;
     }
 
-    public void right(){
-        velX = 1.5;
-        velY= 0;
+    public void setX(int x) {
+        this.x = x;
     }
 
-    public void upStop(){
-        velY = 0;
-        velX = 0;
+    public void setY(int y) {
+        this.y = y;
     }
 
-    public void downStop(){
-        velY = 0;
-        velX = 0;
+    public int getvX() {
+        return vX;
     }
 
-    public void leftStop(){
-        velX = 0;
-        velY = 0;
+    public void setvX(int vX) {
+        this.vX = vX;
     }
 
-    public void rightStop(){
-        velX = 0;
-        velX = 0;
+    public int getvY() {
+        return vY;
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode();
-        if( code == KeyEvent.VK_UP){
-            up();
-        }
-        if(code == KeyEvent.VK_DOWN){
-            down();
-        }
-        if (code == KeyEvent.VK_LEFT){
-            left();
-        }
-        if (code == KeyEvent.VK_RIGHT){
-            right();
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        int code = e.getKeyCode();
-        if(code == KeyEvent.VK_UP){
-            upStop();
-        }
-        if (code == KeyEvent.VK_DOWN){
-            downStop();
-        }
-        if(code == KeyEvent.VK_LEFT){
-            leftStop();
-        }
-
-        if(code == KeyEvent.VK_RIGHT){
-            rightStop();
-        }
-
-
+    public void setvY(int vY) {
+        this.vY = vY;
     }
 }
